45582064881258db50a3992ea1093dbd05381894
Troy Moling <troy.moling@gmail.com>
2018-02-12 22:50:03 -0600

    added css and code tweaks

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Giphy-AJAX-Buttons
+
+If you love Seinfeld and love GIFS, then have I got the game for you. The hours will melt away as YOU type in Seinfeld character after Seinfeld character to reveal GIFS sure to make you the maste of your domain.
+
+Enjoy!
--- javascript/giphy.js
+++ javascript/giphy.js
@@ -1,64 +1,75 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  //create an array of button variables
+	//create an array of button variables
 	var topic = ["Jerry", "Elaine", "George", "Kramer"];
 	displayTopic();
 
-	$('.searchArea').append("<form><input type='text' id='seinfeldInput'></input><input id ='addSeinfeld' type ='submit' value ='Submit'></input></form><br>");
+//search bar...class from html
+//input elements are used within a form element to declare input controls that allow users to input data.
+	$('.searchArea').append("<form><input type='text' id='seinfeldInput'></input><input id ='addSeinfeld' type ='submit' value ='New Character'></input></form><br>");
 
-function displayTopic(){
+	//loop for appending each button in the array
+	function displayTopic() {
 		$('.buttons').empty();
-		for(var i = 0; i < topic.length; i++){
+		for (var i = 0; i < topic.length; i++) {
+			//code this var
 			var thesePretzels = $('<button>')
 			thesePretzels.addClass('festivus');
+
 			thesePretzels.attr('data-name', topic[i]);
 			thesePretzels.text(topic[i]);
 			$('.buttons').append(thesePretzels);
 		}
-}
-
-$('#addSeinfeld').on('click', function(){
-
-  //helps in creating extra button for searching
-  topic.push($('#seinfeldInput').val().trim());
-
-	displayTopic();
-	
-	return false; 
-})
-
-$(document).on('click', '.festivus', showCharacter);
-$(document).on('click', '#gif', animateGIF);
+	}
 
 
-function showCharacter(){
-
-	var yadaYada = $(this).attr('data-name');
-	var newCharacter = yadaYada; 
-	
-  //add in giphy url, key, get method
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q="+newCharacter+"&api_key=sjb4PqrQknVK7mzCOnC9XPlh3klbBEWc"+"&limit=10";
-
-	$(".seinfeldGif").empty();
-
-  //the still command helps in freezing images upon page load
-  $.ajax({url: queryURL, method:'GET'}).done(function(response){
-		for(var i = 0; i < response.data.length; i++){
-			$('.seinfeldGif').prepend("<img src = " + response.data[i].images.downsized_still.url + " id = 'gif' still = " + response.data[i].images.downsized_still.url+" animate = " + response.data[i].images.downsized.url+" data-name = " + newCharacter + "><p>Rating: " + response.data[i].rating +"</p></div> </div>");
-		}
+		//helps in creating extra button for searching
+		//val gets the current value of the first element in the set of matched elements or set the value of every matched element.
+		//trim helps in accounting for extra spaces typed into search
+	$('#addSeinfeld').on('click', function () {
+		topic.push($('#seinfeldInput').val().trim());
+		displayTopic();
+		return false;
 	})
-}
 
-//animate gifs upon clicking
-//stop gifs upon clicking
-function animateGIF(){
-	console.log(this);
-	console.log($(this).attr('still'))
-	if($(this).attr("src") === $(this).attr('animate')){
-		$(this).attr("src", $(this).attr('still'));
+	function showCharacter() {
+
+		var yadaYada = $(this).attr('data-name');
+		var newCharacter = yadaYada;
+
+		//add in giphy url, key, get method
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newCharacter + "&api_key=sjb4PqrQknVK7mzCOnC9XPlh3klbBEWc" + "&limit=10";
+
+		//empty method doesn't accept any arguments
+		$(".seinfeldGif").empty();
+
+		//the still command helps in freezing images upon page load
+		//ajax used here to retrieve data from url database...remember url and GET
+		//response is used for getting a response to the code being sent out...data from AJAX request comes back
+		//use 'then' or 'done' here
+		$.ajax({ url: queryURL, method: 'GET' }).then(function (response) {
+			for (var i = 0; i < response.data.length; i++) {
+				$('.seinfeldGif').prepend("<img src = " + response.data[i].images.downsized_still.url + " id = 'gif' still = " + response.data[i].images.downsized_still.url + " animate = " + response.data[i].images.downsized.url + " data-name = " + newCharacter + "><p>Rating: " + response.data[i].rating + "</p></div> </div>");
+			}
+		})
 	}
-	else{
-		$(this).attr("src", $(this).attr('animate'));
+
+	//call the function
+	$(document).on('click', '.festivus', showCharacter);
+
+	//animate gifs upon clicking
+	//stop gifs upon clicking
+	function stopAndStart() {
+
+		if ($(this).attr("src") === $(this).attr('animate')) {
+			$(this).attr("src", $(this).attr('still'));
+		}
+		else {
+			$(this).attr("src", $(this).attr('animate'));
+		}
 	}
-}
+
+	//calling the function
+	$(document).on('click', '#gif', stopAndStart);
+
 });
